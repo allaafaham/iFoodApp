e851c1d7e9c505168af67b04508d43e7745c9f0c
allaafaham <alaafahham@hotmail.com>
2025-09-13 14:30:44 +0200

    Add Toggle functionality

--- frontend/adminpanel/src/App.jsx
+++ frontend/adminpanel/src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AddFood from './pages/AddFood/AddFood'
+import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Menubar from "./components/Menubar/Menubar.jsx";
+import { Route, Routes } from 'react-router-dom';
+import ListFood from './pages/ListFood/ListFood';
+import Orders from './pages/Orders/Orders';
+
+
 
 const App = () => {
+
+  const [sidebarVisible, setSidebarVisible] = useState(true);
+  const toggleSidebar = () => {
+      setSidebarVisible(!sidebarVisible);
+  }
+
   return (
      <div className="d-flex" id="wrapper">
-            <Sidebar/>
+            <Sidebar sidebarVisible={sidebarVisible}/>
             <div id="page-content-wrapper">
-                <Menubar/>
+                <Menubar toggleSidebar = {toggleSidebar}/>
                 <div className="container-fluid">
                     <Routes>
                         <Route path='/add' element={<AddFood/>}/>
--- frontend/adminpanel/src/components/Sidebar/Sidebar.jsx
+++ frontend/adminpanel/src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {assets} from '../../assets/assets';
 
-const Sidebar = () => {
+const Sidebar = ({sidebarVisible}) => {
   return (
-    <div className="border-end bg-white" id="sidebar-wrapper">
+    <div className={`border-end bg-white ${sidebarVisible ? '': 'd-none'}`} id="sidebar-wrapper">
             <div className="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
             <div className="list-group list-group-flush">
-                <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
-                <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
-                <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
-                <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
-                <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
-                <a className="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/add">Add Food</Link>
+                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/list">List Food</Link>
+                <Link className="list-group-item list-group-item-action list-group-item-light p-3" to="/orders">Overview</Link>
             </div>
     </div>
   )
